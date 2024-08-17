1648d5a73e289968af8e8a80f807d7436690aec3
Martin06270 <martinng06@hotmail.com>
2024-08-17 16:24:15 +0200

    Stelarium

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,57 @@
+
+import { Socials } from "@/constants";
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+        <a
+          href="#about-me"
+          className="h-auto w-auto flex flex-row items-center"
+        >
+          <Image
+            src="/NavLogo.png"
+            alt="logo"
+            width={70}
+            height={70}
+            className="cursor-pointer hover:animate-slowspin"
+          />
+
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            Stelarium
+          </span>
+        </a>
+
+        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+            <a href="#L'histoire" className="cursor-pointer">
+              L'histoire
+            </a>
+            <a href="#Concept art" className="cursor-pointer">
+              Concept art
+            </a>
+            <a href="#Panier" className="cursor-pointer">
+              Panier
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-5">
+          {Socials.map((social) => (
+            <Image
+              src={social.src}
+              alt={social.name}
+              key={social.name}
+              width={24}
+              height={24}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Stelariume
+          </h1>
+        </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+          Plongez dans l'univers de
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              Stelariume.{" "}
+            </span>
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+        Où les étoiles ne sont plus seulement des points lumineux dans le ciel,
+        mais les vestiges de civilisations oubliées. Préparez-vous à explorer les confins de l'espace et à défier les limites de la réalité dans cette épopée de science-fiction inoubliable. 
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          En savoir plus !
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/illustration.jpg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
