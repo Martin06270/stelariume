a92511bac842e1ecc1b9eda41dc8f2b1553973ac
Martin06270 <martinng06@hotmail.com>
2024-08-20 19:30:37 +0200

    Avancement.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/main/Hero";
 import Image from "next/image";
+import AuroraHero from "@/components/main/AuroraHero";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col h-[850px] gap-20">
         <Hero />
+        <AuroraHero/>
       </div>
       Stelariume
     </main>
