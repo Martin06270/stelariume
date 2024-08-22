759edc3fa97f25ce581213c996bd99b634b7e7a1
Martin06270 <martinng06@hotmail.com>
2024-08-22 18:36:48 +0200

    Modification de l'aurorahero

--- components/main/AuroraHero.tsx
+++ components/main/AuroraHero.tsx
@@ -38,11 +38,6 @@ export const AuroraHero = () => {
       className="relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
     >
       <div className="relative z-10 flex flex-col items-center">
-        <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
-        </span>
-        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
-            
-        </h1>
         <motion.button
           style={{
             border,
@@ -59,12 +54,6 @@ export const AuroraHero = () => {
           Acheter le roman 
           {/* <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" /> */}
         </motion.button>
-      </div>
-
-      <div className="absolute inset-0 z-0">
-        <Canvas>
-          <Stars radius={50} count={2500} factor={4} fade speed={2} />
-        </Canvas>
       </div>
     </motion.section>
   );
